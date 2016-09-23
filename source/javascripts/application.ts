f2531b061851de88dadcedda014a996a614c9212
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-22 21:28:54 -0500

    Added pokedex module bootstrap on dom content loaded to main application ts

--- source/javascripts/application.ts
+++ source/javascripts/application.ts
@@ -1,1 +1,8 @@
-// application.js
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { PokedexModule }          from './pokedex/module.ts';
+
+const platform = platformBrowserDynamic();
+
+document.addEventListener('DOMContentLoaded', () =>
+  platform.bootstrapModule(PokedexModule)
+);

0dc7fe3139b762d9dd3bf2ab628f6fdbfb158fb9
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-25 18:45:10 -0500

    Added pokemon service to pokemon module providers

--- source/javascripts/pokemon/module.ts
+++ source/javascripts/pokemon/module.ts
@@ -1,10 +1,11 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule }     from '@angular/http';
+import { PokemonService } from './service';
 
 @NgModule({
   imports:   [HttpModule],
-  providers: []
+  providers: [PokemonService]
 })
 
 export class PokemonModule { }

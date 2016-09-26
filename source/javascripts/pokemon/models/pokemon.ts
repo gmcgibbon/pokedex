9caf471a6e35ada77bd58292a7e32e15ff8d89c4
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-26 02:15:25 -0500

    Added typed properties to pokemon model, updated detail partial with more properties

--- source/javascripts/pokemon/models/pokemon.ts
+++ source/javascripts/pokemon/models/pokemon.ts
@@ -1,6 +1,19 @@
+import { SpriteSet }   from './sprite-set';
+import { SlottedType } from './slotted-type';
+
 export class Pokemon {
+
+  constructor() {
+    this.sprites = new SpriteSet();
+    this.types   = [];
+  }
+
   id:     number;
+  name:   string;
+
   height: number;
   weight: number;
-  name:  string;
+
+  sprites: SpriteSet;
+  types:   SlottedType[];
 }

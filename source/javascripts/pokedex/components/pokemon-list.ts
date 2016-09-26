b102e8633cd8aedfd5880970f8cb96b24d92357a
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-26 01:36:46 -0500

    Minor refactor on pokemon-list loading methods, added lazy loading

--- source/javascripts/pokedex/components/pokemon-list.ts
+++ source/javascripts/pokedex/components/pokemon-list.ts
@@ -10,21 +10,35 @@ import { Pokemon }        from '../../pokemon/models/pokemon';
 
 export class PokedexPokemonList implements OnInit {
 
-  pokemons:   Pokemon[] = [];
-  startFrom:  number    = 1;
-  startCount: number    = 15;
+  pokemons:    Pokemon[];
+  startCount:  number;
+  updateCount: number;
 
-  constructor(private pokedex : PokedexService) { }
+  constructor(private pokedex : PokedexService) {
+    this.pokemons    = [];
+    this.startCount  = 15;
+    this.updateCount = 5;
+  }
 
   ngOnInit(): void {
-    this.pokedex.pokemon.getEach(this.startFrom, this.startCount).subscribe(
-      pokemon => { this.pokemons.push(pokemon); },
-      error   => { console.log(error); },
-      ()      => { this.pokedex.loading = false; }
-    );
+    this.load(this.startCount);
   }
 
   show(pokemon : Pokemon) : void {
     this.pokedex.onSelected.emit(pokemon);
+  }
+
+  more() : void {
+    this.load(this.updateCount);
+  }
+
+  private load(count : number) : void {
+    let start = this.pokemons.length + 1;
+    this.pokedex.loading = true;
+    this.pokedex.pokemon.getEach(start, count).subscribe(
+      pokemon => { this.pokemons.push(pokemon); },
+      error   => { console.log(error); },
+      ()      => { this.pokedex.loading = false; }
+    );
   }
 }

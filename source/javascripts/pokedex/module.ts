e0d2c180deec49166a2f70043900cb14f4a495b1
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-26 01:15:18 -0500

    Added pokemon-detail component, added module declaration and provider for pokemon-detail and pokedex service

--- source/javascripts/pokedex/module.ts
+++ source/javascripts/pokedex/module.ts
@@ -1,20 +1,24 @@
-import { NgModule }           from '@angular/core';
-import { BrowserModule }      from '@angular/platform-browser';
-import { PokemonModule }      from '../pokemon/module';
-import { PokedexComponent }   from './components/main';
-import { PokedexPokemonList } from './components/pokemon-list';
-import { CapitalizePipe }     from './pipes/capitalize'
-import { FixedPipe }          from './pipes/fixed'
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { PokemonModule }        from '../pokemon/module';
+import { PokedexService }       from './service';
+import { PokedexComponent }     from './components/pokedex';
+import { PokedexPokemonList }   from './components/pokemon-list';
+import { PokedexPokemonDetail } from './components/pokemon-detail';
+import { CapitalizePipe }       from './pipes/capitalize'
+import { FixedPipe }            from './pipes/fixed'
 
 @NgModule({
   imports:      [BrowserModule, PokemonModule],
   declarations: [
     PokedexComponent,
     PokedexPokemonList,
+    PokedexPokemonDetail,
     CapitalizePipe,
     FixedPipe
   ],
-  bootstrap:    [PokedexComponent]
+  providers: [PokedexService],
+  bootstrap: [PokedexComponent]
 })
 
 export class PokedexModule { }

5ce01066586c527efbebf9bf93412923911cd7cb
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-25 20:16:26 -0500

    Added pokemon-list component to pokedex module, added pokemon-list partial and pokemon-list element to pokedex partial

--- source/javascripts/pokedex/module.ts
+++ source/javascripts/pokedex/module.ts
@@ -1,11 +1,12 @@
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { PokemonModule }    from '../pokemon/module';
-import { PokedexComponent } from './components/main';
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+import { PokemonModule }      from '../pokemon/module';
+import { PokedexComponent }   from './components/main';
+import { PokedexPokemonList } from './components/pokemon-list';
 
 @NgModule({
   imports:      [BrowserModule, PokemonModule],
-  declarations: [PokedexComponent],
+  declarations: [PokedexComponent, PokedexPokemonList],
   bootstrap:    [PokedexComponent]
 })
 

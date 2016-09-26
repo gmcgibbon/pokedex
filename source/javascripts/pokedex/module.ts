ba99bcdb49ab5ec648aa42106168890563d2d27d
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-25 21:30:43 -0500

    Added pipes to pokedex module, used pipes in pokemon-list

--- source/javascripts/pokedex/module.ts
+++ source/javascripts/pokedex/module.ts
@@ -3,10 +3,17 @@ import { BrowserModule }      from '@angular/platform-browser';
 import { PokemonModule }      from '../pokemon/module';
 import { PokedexComponent }   from './components/main';
 import { PokedexPokemonList } from './components/pokemon-list';
+import { CapitalizePipe }     from './pipes/capitalize'
+import { FixedPipe }          from './pipes/fixed'
 
 @NgModule({
   imports:      [BrowserModule, PokemonModule],
-  declarations: [PokedexComponent, PokedexPokemonList],
+  declarations: [
+    PokedexComponent,
+    PokedexPokemonList,
+    CapitalizePipe,
+    FixedPipe
+  ],
   bootstrap:    [PokedexComponent]
 })
 

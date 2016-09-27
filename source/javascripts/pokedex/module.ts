1d907d8dc4ae344aed6028b84164b150077919fc
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-09-27 01:06:44 -0500

    Added angular2 button, card, and list modules to main pokedex module

--- source/javascripts/pokedex/module.ts
+++ source/javascripts/pokedex/module.ts
@@ -1,15 +1,24 @@
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
+import { MdButtonModule }       from '@angular2-material/button';
+import { MdCardModule }         from '@angular2-material/card';
+import { MdListModule }         from '@angular2-material/list';
 import { PokemonModule }        from '../pokemon/module';
 import { PokedexService }       from './service';
 import { PokedexComponent }     from './components/pokedex';
 import { PokedexPokemonList }   from './components/pokemon-list';
 import { PokedexPokemonDetail } from './components/pokemon-detail';
-import { CapitalizePipe }       from './pipes/capitalize'
-import { FixedPipe }            from './pipes/fixed'
+import { CapitalizePipe }       from './pipes/capitalize';
+import { FixedPipe }            from './pipes/fixed';
 
 @NgModule({
-  imports:      [BrowserModule, PokemonModule],
+  imports:      [
+    BrowserModule,
+    PokemonModule,
+    MdButtonModule.forRoot(),
+    MdCardModule.forRoot(),
+    MdListModule.forRoot()
+  ],
   declarations: [
     PokedexComponent,
     PokedexPokemonList,
